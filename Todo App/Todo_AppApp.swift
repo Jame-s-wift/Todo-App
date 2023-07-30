4a81e1ad5ae088551b9f123d52cabeed63b69c44
James Toh <jtyflol@gmail.com>
2023-07-30 12:50:18 +0800

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by James Toh on 30/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
