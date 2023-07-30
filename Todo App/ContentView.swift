d3fdcfadc5f1ec7757a70bda8fce1f09088187b9
James Toh <jtyflol@gmail.com>
2023-07-30 13:16:47 +0800

    Done with todos!!!!

--- Todo App/ContentView.swift
+++ Todo App/ContentView.swift
@@ -10,14 +10,22 @@ import SwiftUI
 struct ContentView: View {
     
     @State private var todos = [
-        Todo(title: "Buy 20kg Nutella"),
+        Todo(title: "Buy 20kg Nutella", isCompleted: true),
         Todo(title: "Eat 20kg Nutella")
     ]
     
     var body: some View {
         NavigationStack {
-            List(todos) { todo in
-                Text(todo.title)
+            List($todos) { $todo in
+                HStack{
+                    Image(systemName: todo.isCompleted ? "checkmark.circle.fill" : "circle")
+                        .onTapGesture {
+                            todo.isCompleted.toggle()
+                        }
+                    
+                    Text(todo.title)
+                        .strikethrough(todo.isCompleted)
+                }
             }
             .navigationTitle("Todos")
         }
